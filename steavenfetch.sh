9f800d1df664013ded571080e70209a3c99beffc
SteavenGamerYT <62666559+SteavenGamerYT@users.noreply.github.com>
2023-05-07 16:37:49 +0300

    Update steavenfetch.sh

--- steavenfetch.sh
+++ steavenfetch.sh
@@ -3,7 +3,7 @@ echo "DS: $XDG_SESSION_TYPE"
 echo "Shell: $0"
 echo -n "Kernel: " && uname -r
 echo -n "Model: " && sudo dmidecode -s system-product-name
-echo -n "Serial Number: " sudo dmidecode -s system-serial-number
+echo -n "Serial Number: " && sudo dmidecode -s system-serial-number
 echo -n "CPU: " && lscpu | grep "Model name:" | sed -r 's/Model name:\s{1,}//g'
 echo -n "GPU: " && lspci | grep -i VGA
 echo -n "RAM: " && grep MemTotal /proc/meminfo
