1e95258677fb479142a3009cbb6c46b8913195b3
SteavenGamerYT <62666559+SteavenGamerYT@users.noreply.github.com>
2023-05-07 16:31:23 +0300

    Update steavenfetch.sh

--- steavenfetch.sh
+++ steavenfetch.sh
@@ -2,5 +2,5 @@ echo "DE: $XDG_SESSION_DESKTOP"
 echo "DS: $XDG_SESSION_TYPE"
 echo "Shell: $0"
 lscpu | grep "Model name:" | sed -r 's/Model name:\s{1,}//g'
-grep MemTotal /proc/meminfo
 lspci | grep -i VGA
+grep MemTotal /proc/meminfo
