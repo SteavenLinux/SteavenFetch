7aa971c9fa0fcf6c599948c7d61e95df22f5c754
SteavenGamerYT <omarhanykasban706@gmail.com>
2025-07-25 01:56:36 +0300

    build

---
+++ SteavenFetch/target/release/SteavenFetch.d
@@ -0,0 +1,1 @@
+/home/omarhanykasban/GitHub/SteavenLinux/steavenfetch/SteavenFetch/target/release/SteavenFetch: /home/omarhanykasban/GitHub/SteavenLinux/steavenfetch/SteavenFetch/src/main.rs
---
+++ SteavenFetch/target/release/deps/SteavenFetch-e7bb08e278ab4e71.d
@@ -0,0 +1,5 @@
+/home/omarhanykasban/GitHub/SteavenLinux/steavenfetch/SteavenFetch/target/release/deps/SteavenFetch-e7bb08e278ab4e71: src/main.rs
+
+/home/omarhanykasban/GitHub/SteavenLinux/steavenfetch/SteavenFetch/target/release/deps/SteavenFetch-e7bb08e278ab4e71.d: src/main.rs
+
+src/main.rs:
